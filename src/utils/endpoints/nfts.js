48d0ec74ff37ba3695b06aa7b2bac1c8366ef663
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-12-08 23:32:33 +0100

    feat: fetch NFTs only

---
+++ src/utils/endpoints/nfts.js
@@ -0,0 +1,66 @@
+import { evaluateOracleState } from "../arweave/exm-rwx.js";
+import { getNearNfts, getMoralisNfts, getEvmosNfts } from "../server-utils.js";
+import { getAddrCheckSum } from "../evm/web3.js";
+import base64url from "base64url";
+
+export async function getNftsOf(network, address) {
+  try {
+    let userProfile;
+    const responseProfile = {};
+    const state = (await evaluateOracleState())?.identities;
+    if (!state) {
+      return "e30";
+    }
+
+    if (!["arweave", "evm", "exotic"].includes(network)) {
+      return "e30";
+    }
+
+    if (network === "arweave") {
+      userProfile = state.find(
+        (user) => user["arweave_address"] === address && !!user.is_verified
+      );
+    } else {
+      userProfile = state.find(
+        (user) => user["primary_address"] === address && !!user.is_verified
+      );
+    }
+
+    if (!userProfile) {
+      return "e30";
+    }
+
+    const verifiedEvmAddresses = userProfile.addresses.filter(
+      (addr) => !!addr.is_verified && addr.ark_key === "EVM"
+    );
+    const verifiedNearAddresses = userProfile.addresses.filter(
+      (addr) =>
+        !!addr.is_verified &&
+        addr.ark_key === "EXOTIC" &&
+        addr.network === "NEAR-MAINNET"
+    );
+    responseProfile.EVM = [];
+    responseProfile.NEAR = [];
+
+    for (const linkage of verifiedEvmAddresses) {
+      const address = await getAddrCheckSum(linkage.address);
+      const nftsErc = await getMoralisNfts(address);
+      const nftsEvmos = await getEvmosNfts(address);
+      responseProfile.EVM =
+        responseProfile.EVM.concat(nftsErc).concat(nftsEvmos);
+    }
+
+    for (const linkage of verifiedNearAddresses) {
+      const nfts = await getNearNfts(linkage.address);
+      responseProfile.NEAR = responseProfile.NEAR.concat(nfts);
+    }
+
+    responseProfile.EVM.flat();
+    responseProfile.NEAR.flat();
+
+    return base64url(JSON.stringify(responseProfile));
+  } catch (error) {
+    console.log(error);
+    return "e30";
+  }
+}
